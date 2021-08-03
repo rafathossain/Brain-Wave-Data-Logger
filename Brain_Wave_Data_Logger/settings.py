694dc7d8559a11263c445e9b774643a1cf6020ed
Md. Rafat Hossain <rafathossain96@gmail.com>
2021-08-04 01:41:59 +0600

    Update settings.py

--- Brain_Wave_Data_Logger/settings.py
+++ Brain_Wave_Data_Logger/settings.py
@@ -135,10 +135,4 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'public/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public/static')
